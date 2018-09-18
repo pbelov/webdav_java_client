64f4c883d2a090ac043472fbfd070bd444a14066
Pavel Belov <pavel.belov@kofax.com>
2018-09-18 20:59:07 +0300

    update uploading

---
+++ build.sh
@@ -0,0 +1,3 @@
+rm pbelov_webdav_client.jar
+mvn package
+mv target/pbelov_webdav_client-jar-with-dependencies.jar pbelov_webdav_client.jar
--- src/com/pbelov/java/webdav_client/Main.java
+++ src/com/pbelov/java/webdav_client/Main.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Main {
     public static boolean DEBUG = false;
@@ -26,8 +27,12 @@ public class Main {
 
     private String from, to, host, user, pass;
 
+    private final String pattern = Pattern.quote(System.getProperty("file.separator"));
+    private final String WEB_SEPARATOR = "/";
+
+
     public Main(String[] args) {
-        Utils.println("", "Webdav Java Client v.0.2.1");
+        Utils.println("", "Webdav Java Client v.0.2.2");
         if (args == null || args.length == 0) {
             System.out.println("Usage\n");
             Utils.println("upload -from [from] -to [to] -host [protocol://host:port] -user [user] -pass [password]");
@@ -65,8 +70,8 @@ public class Main {
     }
 
     private void goList(Sardine sardine) throws IOException {
-        Utils.println("List of " + host + "/" + from + ": \r\n");
-        List<DavResource> list = sardine.list(host + "/" + from);
+        Utils.println("List of " + host + WEB_SEPARATOR + from + ": \r\n");
+        List<DavResource> list = sardine.list(host + WEB_SEPARATOR + from);
         for (DavResource davResource : list) {
             Utils.println(davResource.getName());
         }
@@ -74,10 +79,44 @@ public class Main {
 
     private void goUpload(Sardine sardine) throws IOException {
         File[] files = getFiles();
-        for (File file : files) {
-            System.out.println(file.getName());
-            byte[] bytes = Files.readAllBytes(file.toPath());
-            sardine.put(host + "/" + to + "/" + file.getName(), bytes);
+        uploadFiles(sardine, files);
+    }
+
+    private void uploadFiles(Sardine sardine, File[] localFiles) throws IOException {
+        if (localFiles != null) {
+            for (File localFile : localFiles) {
+                if (localFile.isFile()) {
+                    uploadFile(sardine, localFile);
+                } else {
+                    uploadFiles(sardine, localFile.listFiles());
+                }
+            }
+        }
+    }
+
+    private void uploadFile(Sardine sardine, File localFile) throws IOException {
+        Utils.println("Upload: " + localFile);
+        byte[] bytes = Files.readAllBytes(localFile.toPath());
+        createRemoteDirectory(sardine, host + WEB_SEPARATOR + to);
+        createRemoteDirectories(sardine, localFile.getParentFile().getPath());
+
+        sardine.put(host + WEB_SEPARATOR + to + WEB_SEPARATOR + localFile.getParent().replaceAll(pattern, WEB_SEPARATOR) + WEB_SEPARATOR + localFile.getName(), bytes);
+    }
+
+    private void createRemoteDirectory(Sardine sardine, String path) throws IOException {
+        if (!sardine.exists(path)) {
+            sardine.createDirectory(path);
+        }
+    }
+
+    private void createRemoteDirectories(Sardine sardine, String path) throws IOException {
+        String[] pathDirs = path.split(pattern);
+        StringBuilder sb = new StringBuilder();
+        for (String pathDir : pathDirs) {
+            sb.append(pathDir);
+            String remoteDirPath = host + WEB_SEPARATOR + to + WEB_SEPARATOR + sb.toString();
+            createRemoteDirectory(sardine, remoteDirPath);
+            sb.append(WEB_SEPARATOR);
         }
     }
 
@@ -93,11 +132,11 @@ public class Main {
             final String resourceName = davResource.getName();
             final String resourcePath = davResource.getPath();
             if (!davResource.isDirectory()) {
-                InputStream is = sardine.get(host + "/" + resourcePath);
+                InputStream is = sardine.get(host + WEB_SEPARATOR + resourcePath);
                 File targetFile = new File(FileUtils.WORKING_DIR, to);
                 FileUtils.checkFile(targetFile);
                 targetFile.mkdir();
-                String targetPath = targetFile.getPath() + File.separator + davResource.getPath().substring(from.length() + 2).replaceAll("/", "\\" + File.separator).replaceAll(resourceName, "");
+                String targetPath = targetFile.getPath() + File.separator + davResource.getPath().substring(from.length() + 2).replaceAll(WEB_SEPARATOR, "\\" + File.separator).replaceAll(resourceName, "");
                 targetFile = new File(targetPath, resourceName);
                 FileUtils.saveFile(is, targetFile);
             } else {
@@ -108,7 +147,7 @@ public class Main {
     }
 
     private List<DavResource> getListFrom(Sardine sardine, String dir) throws IOException {
-        return sardine.list(host + "/" + dir);
+        return sardine.list(host + WEB_SEPARATOR + dir);
     }
 
     private File[] getFiles() {
